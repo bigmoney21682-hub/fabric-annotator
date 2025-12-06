aa2f7133c54bfd3d3c80fc0fece3aed040e82e64
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-06 06:39:16 -0500

    Update service-worker.js

--- demos/service-worker.js
+++ demos/service-worker.js
@@ -4,15 +4,13 @@ const urlsToCache = [
   '/fabric-annotator/manifest.json',
   '/fabric-annotator/icons/icon-192.png',
   '/fabric-annotator/icons/icon-512.png',
-  '/fabric-annotator/style.css',  // optional, if you have a separate CSS file
-  '/fabric-annotator/fabric.min.js' // optional if you’re using local Fabric.js
+  '/fabric-annotator/fabric.min.js'
 ];
 
 // Install event: cache files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
@@ -20,18 +18,16 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
+      Promise.all(keys.filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
       )
     )
   );
 });
 
-// Fetch event: respond with cached files when offline
+// Fetch event: respond with cache first
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
