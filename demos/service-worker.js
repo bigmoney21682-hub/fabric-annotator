d8c4cb5762bb77068ce694e8874871927255d256
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-06 09:49:04 -0500

    Update service-worker.js

--- demos/service-worker.js
+++ demos/service-worker.js
@@ -1,24 +1,57 @@
+// Service Worker for FieldAR PWA
 const CACHE_NAME = 'fieldar-cache-v1';
+
+// Files relative to /demos/
 const urlsToCache = [
-  '/fabric-annotator/fieldar.html',
-  '/fabric-annotator/manifest.json',
-  '/fabric-annotator/icons/icon-192.png',
-  '/fabric-annotator/icons/icon-512.png',
-  '/fabric-annotator/fabric.min.js'
+  './fieldar.html',
+  './manifest.json',
+  '../dist/fab-annotator.js',
+  '../dist/fab-annotator.css',
+  '../icons/Blue_GCPA-removebg-preview.png',
+  '../icons/Blue_He_Comp.png'
 ];
 
+// Install SW + cache files
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('activate', event => {
+  console.log('[SW] Install event');
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[SW] Caching app files');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.error('[SW] Cache addAll error:', err))
   );
 });
 
+// Activate SW + remove old caches
+self.addEventListener('activate', event => {
+  console.log('[SW] Activate event');
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => {
+            console.log('[SW] Removing old cache:', key);
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+});
+
+// Intercept requests
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Serve from cache OR network
+        return response || fetch(event.request);
+      })
+      .catch(err => {
+        console.error('[SW] Fetch failed:', err);
+        return fetch(event.request);
+      })
+  );
 });
